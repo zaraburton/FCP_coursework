dfa7861038bde5aca7bda9b4ad127c3f299bf683
HannahSpurgeon <73698372+HannahSpurgeon@users.noreply.github.com>
2021-04-12 10:38:38 +0100

    1. Creating a function to plot the shortest path from an entrance node to a till node in blue and other nodes in red (line 51-59) 2. Attempting to set node names to represent the area of the shop they represent rather than x,y co-ordinates using set_node_attributes. Managed to set the nodes to a a-z variable (see code line 29-30) but struggling to get it to work for a self written list.

--- Walk_simulation/plots/path_generator.py
+++ Walk_simulation/plots/path_generator.py
@@ -3,6 +3,7 @@ import pandas as pd
 import copy
 import matplotlib as plt
 import matplotlib.pyplot as plt
+import string
 '''Function to create lists of the paths between tills and the entrance from G'''
 def add_edge_to_graph(G, e1, e2, w):
     G.add_edge(e1, e2, weight=w)
@@ -16,20 +17,47 @@ for i in range(len(edges)):
     add_edge_to_graph(G, points[edges[i][0]], points[edges[i][1]], edges[i][2])
 pos = {point: point for point in points}
 
-print(" The number of nodes: {:d}".format(len(G.nodes())))
-print(" The number of edges: {:d}".format(len(G.edges())))
+#setting node characteristics, defining till and entrance nodes
+till_nodes = [(10,0),(4,0)]
+entrance_nodes = [(10,1)]
+aisle_nodes = [(10,10), (10,4), (7,10),(7,7),(7,5),(7,3),(4,7),(4,3),(1,7),(1,4), (4,10), (1,10)]
+
+#relabelling the nodes as names of what they represent
+mapping= { 0: "entrance",1: "aisle2",2: "aisle1",3:"aisle3",4:"aisle4",5:"aisle5",6:"aisle6",7:"aisle7",8:"aisle8",9:"till1",10:"aisle9",11:"aisle10",12:"till2",13:"aisle11",14:"aisle12"}
+G = nx.relabel_nodes(G, mapping)
+
+#mapping = dict(zip(G, string.ascii_lowercase))
+#G = nx.relabel_nodes(G, mapping)
+
+def set_node_characteristic(G,n,value):
+    ''' Where the G and n are the graph and node that are being assigned a value relating to the time spent at the node'''
+    return nx.set_node_attributes(G,n,value)
+
+nx.set_node_attributes(G, {0:5, 8:5}, 'time')
+G.nodes(data=True)
+till_nodes = set_node_characteristic(G,till_nodes,5)
+entrance_nodes = set_node_characteristic(G,entrance_nodes,5)
+aisle_nodes = set_node_characteristic(G,aisle_nodes,1)
+
+print(G.nodes.data)
 
 #this is the function to get the paths between the nodes, n1 is entrance node, n2 is exit node
-def paths_entrance_to_till(G,n1,n2):
-    nx.all_simple_paths(G,n1,n2)
+def path_entrance_to_till(G,n1,n2):
+    return nx.shortest_path(G,n1,n2)
 
-paths1 = nx.all_simple_paths(G, (10,0),(4,0))
-paths2 = nx.all_simple_paths(G, (10,0),(1,0))
-#printing the list of the paths
-print(list(paths1))
-print(list(paths2))
-#plotting path1 nodes as blue for visulisation
-node_colors = ["blue" if n in list(paths1) else "red" for n in G.nodes()]
-nx.draw_networkx_nodes(G, pos=pos, node_color=node_colors)
+paths1 = path_entrance_to_till(G, (10,0),(4,0))
+paths2 = path_entrance_to_till(G, (10,0),(1,0))
+
+#function to enable plotting of the shortest path in BLUE
+def node_colors(G, path):
+    colours = []
+    for node in G.nodes():
+        if node in path:
+            colours.append('b')
+        else:
+            colours.append('r')
+    return colours
+colours = node_colors(G,paths2)
+nx.draw_networkx_nodes(G, pos=pos, node_color=colours)
 nx.draw_networkx_edges(G, pos=pos)
 plt.show()
