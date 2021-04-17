7a38ee37be3a7ab53337d2b8315c632a7b76ca10
HannahSpurgeon <73698372+HannahSpurgeon@users.noreply.github.com>
2021-04-17 13:56:57 +0100

    Animation function attempt

--- Networkx_movement/animation.py
+++ Networkx_movement/animation.py
@@ -20,26 +20,50 @@ import Networkx_aldi_layout as lay
 # Import aldi layout for other file
 import Initial_Simulation as sim
 
-# pulling graph from aldilayout
+# pulling graph from aldi_layout
 G = lay.aldi_layout()
 
+# default time
+time_step = 1
+
 class NetworkAnimation:
-    ''' Animate network showing movement of people'''
+    ''' Animate networkX showing movement of people'''
     def __init__(self,axes,sim):
         self.axes = axes
         self.sim = sim
-#function to enable the animation of movement from person
-# i is the number of frames the animation needs
-# a frame is like a still image of the network at each point in sim
-# defining list of colours for the graph
+        self.image = self.axes.imshow(network)
+
+    def init(self):
+        return self.update(0)
+
+#function to update the frame based on the number of frames from sim.timestep
+    def update(self, frames):
+        sim.time = frames
     colours = ['r', 'b', 'g', 'y', 'w', 'm']
+
+    # function to enable the animation of movement from person
+    # i is the number of frames the animation needs
+    # a frame is like a still image of the network at each point in sim
     def animate(self, i):
-        nx.draw_networkx(G,pos = sim.pos,node_color = [random.choice(colors) for j in range(9)])
-    nx.draw(G)
-    fig = plt.gcf()
+        network = nx.draw_networkx(sim.G,pos = sim.pos,node_color = [random.choice(colors) for j in range(9)])
+        return network
+
 # anim repeated calls animate function, incrementing i in each iteration
 # frames defines the number of times we call animate - should be time increment relating to sim when we have it
 #20 for now as no time in sim class
 # interval is the interval between each call, default set to 20 for now
 # blit true just makes sure we only change the network parts that have change
-    anim = animation.FuncAnimation(fig,animate,frames = 20 , interval =20 , blit=True)
+    anim = animation.FuncAnimation(sim.G,animate,frames = 20 , interval =20 , blit=True)
+
+#function to plot simulation
+    def plot_simulation(sim, time):
+
+        fig = plt.gcf()
+# assuming simulation will have an update function that updates shop network
+        while sim.time < sim.time:
+            sim.update()
+        network = nx.draw_networkx(G)
+        network.set_title('Aldi simulation of COVID-19 spreading')
+
+        return network
+
