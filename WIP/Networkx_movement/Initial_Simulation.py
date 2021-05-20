ed318b54794a09be6451e97e720ba16212cd0951
HannahSpurgeon <73698372+HannahSpurgeon@users.noreply.github.com>
2021-05-20 11:37:35 +0100

    Removing user input option

--- WIP/Networkx_movement/Initial_Simulation.py
+++ WIP/Networkx_movement/Initial_Simulation.py
@@ -23,7 +23,7 @@ def main(args):
     parser = argparse.ArgumentParser(description='Animate an epidemic')
     parser.add_argument('--max_entry', metavar='N', type=int, default=5,
                         help='Maximum of N people can enter at once')
-    parser.add_argument('--duration', metavar='N', type=int, default=200,
+    parser.add_argument('--duration', metavar='N', type=int, default=50,
                         help='Run simulation for N time steps')
     parser.add_argument('--max_shoppers', metavar='N', type=int, default=30,
                     help='Maximum number of shoppers in the shop')
@@ -35,36 +35,22 @@ def main(args):
                     help='Probability of any individual in the area being infected')
     parser.add_argument('--plot', action='store_true',
                     help='Generate plots instead of animation')
-    parser.add_argument('--prompt', action='store_true',
-                    help='Prompts for input arguments and retrieve animation')
     args = parser.parse_args(args)
 
     #TD: make parser args for next variables
 
     #TD: something to say you cant have level of covid and month given at same time? or just pick on or the other? (Will automatically go by the month if thats given)
-    # set level_of_covid_in_area (prob of infected person entering the shop) as infection rate for that month
-    #if args.month:
-    #    level_of_covid_in_area = inf_rate.infection_rate(args.month)
-    #else:
-    #    level_of_covid_in_area = args.level_of_covid
-
-    #if user wants to be prompted
-    if args.prompt:
-        max_entry, duration, max_shoppers, path_system, month = get_user_input()
-        level_of_covid_in_area = inf_rate.infection_rate(month)
-        prob_of_i = 0.2
-        chance_person_wears_mask = 0.4
-        sim = simulation(max_entry, duration, max_shoppers, path_system, prob_of_i,chance_person_wears_mask,level_of_covid_in_area)
-        sim.add_new_shopper(1)
-        results(sim, duration)
+    #set level_of_covid_in_area (prob of infected person entering the shop) as infection rate for that month
+    if args.month:
+        level_of_covid_in_area = inf_rate.infection_rate(args.month)
     else:
-        #running the simulation based off of argument inputs instead
-        prob_of_i = 0.2
-        chance_person_wears_mask = 0.4
         level_of_covid_in_area = args.level_of_covid
-        sim = simulation(args.max_entry, args.duration, args.max_shoppers, args.path_system, prob_of_i,chance_person_wears_mask, level_of_covid_in_area)
-        sim.add_new_shopper(1)
-        results(sim, args.duration)
+        #running the simulation based off of argument inputs instead
+    prob_of_i = 0.2
+    chance_person_wears_mask = 0.4
+    sim = simulation(args.max_entry, args.duration, args.max_shoppers, args.path_system, prob_of_i,chance_person_wears_mask, level_of_covid_in_area)
+    sim.add_new_shopper(1)
+    results(sim, args.duration)
 
     #plotting the graphs or animation based off of user input showing simulation results
     if args.plot:
@@ -74,6 +60,8 @@ def main(args):
     # calling the animation class to plot the animation if the user hasnt specified they want the plots
         animation = Animation(sim, args.duration)
         animation.show()
+
+
 
 
 #----------------------------------------------------------------------------#
